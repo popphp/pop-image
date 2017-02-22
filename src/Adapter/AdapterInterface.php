ec005f814450417b69e138e4ac4bad6a5a4e492f
Nick Sagona <nick@nolainteractive.com>
2017-02-22 10:24:25 -0600

    Update Copyright

--- src/Adapter/AdapterInterface.php
+++ src/Adapter/AdapterInterface.php
@@ -4,7 +4,7 @@
  *
  * @link       https://github.com/popphp/popphp-framework
  * @author     Nick Sagona, III <dev@nolainteractive.com>
- * @copyright  Copyright (c) 2009-2016 NOLA Interactive, LLC. (http://www.nolainteractive.com)
+ * @copyright  Copyright (c) 2009-2017 NOLA Interactive, LLC. (http://www.nolainteractive.com)
  * @license    http://www.popphp.org/license     New BSD License
  */
 
@@ -27,7 +27,7 @@ use Pop\Image\Type;
  * @category   Pop
  * @package    Pop\Image
  * @author     Nick Sagona, III <dev@nolainteractive.com>
- * @copyright  Copyright (c) 2009-2016 NOLA Interactive, LLC. (http://www.nolainteractive.com)
+ * @copyright  Copyright (c) 2009-2017 NOLA Interactive, LLC. (http://www.nolainteractive.com)
  * @license    http://www.popphp.org/license     New BSD License
  * @version    3.0.0
  */
@@ -165,7 +165,7 @@ interface AdapterInterface
      * @return AdapterInterface
      */
     public function setType(Type\TypeInterface $type);
-    
+
     /**
      * Load the image resource from the existing image file
      *
